b34e41cfdd7530a998211d1d3bead04e4571e7e6
Neil-Player456 <Nmrubix11@gmail.com>
2025-02-20 05:44:21 +0000

    Need some help

--- src/app.js
+++ src/app.js
@@ -5,24 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   const generateGame = () => {
     const showArray = ["DanDaDan", "My Hero Acadamia", "Attack On Titan", "Invincible"];
     const placesArray = ["Outside", "Bedroom", "Up Stairs", "Down Stairs", "Back Yard"];
     const partnerArray = ["Father", "Mother", "Sister", "Brother", "Wife", "Husband"];
-  
+
     let showIndex = Math.floor(Math.random() * showArray.length)
-    let placesIndex  = Math.floor(Math.random() * placesArray.length)
-    let partnerIndex  = Math.floor(Math.random() * partnerArray.length)
-  
+    let placesIndex = Math.floor(Math.random() * placesArray.length)
+    let partnerIndex = Math.floor(Math.random() * partnerArray.length)
+
     const show = document.querySelector(".show")
     const places = document.querySelector(".places")
     const partner = document.querySelector(".partner")
-  
+
     show.innerHTML = showArray[showIndex]
     places.innerHTML = placesArray[placesIndex]
-    partner.innerHTML =partnerArray[nameIndex]
-  
+    partner.innerHTML = partnerArray[nameIndex]
+
   }
   console.log("Hello Rigo from the console!");
 };
