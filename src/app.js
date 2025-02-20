ef635a4d7fb06d312c2b2c7bfe3261d857c91e4d
Neil-Player456 <Nmrubix11@gmail.com>
2025-02-20 04:51:11 +0000

    Final changes

--- src/app.js
+++ src/app.js
@@ -11,17 +11,17 @@ window.onload = function() {
     const placesArray = ["Outside", "Bedroom", "Up Stairs", "Down Stairs", "Back Yard"];
     const partnerArray = ["Father", "Mother", "Sister", "Brother", "Wife", "Husband"];
   
-    let showIndex = Math.floor(Math.random() * equipmentArray.length)
-    let placesIndex  = Math.floor(Math.random() * locationArray.length)
-    let partnerIndex  = Math.floor(Math.random() * nameArray.length)
+    let showIndex = Math.floor(Math.random() * showArray.length)
+    let placesIndex  = Math.floor(Math.random() * placesArray.length)
+    let partnerIndex  = Math.floor(Math.random() * partnerArray.length)
   
     const show = document.querySelector(".show")
     const places = document.querySelector(".places")
     const partner = document.querySelector(".partner")
   
-    equipment.innerHTML = equipmentArray[equipmentIndex]
-    location.innerHTML = locationArray[locationIndex]
-    name.innerHTML = nameArray[nameIndex]
+    show.innerHTML = showArray[showIndex]
+    places.innerHTML = placesArray[placesIndex]
+    partner.innerHTML =partnerArray[nameIndex]
   
   }
   console.log("Hello Rigo from the console!");
